656f36d85eb1f8f2d8ba342d5696b0cac97f2c3d
dabin7 <brian421@naver.com>
2022-09-12 21:15:35 +0900

    Update Td.jsx

--- tictactoe/src/Td.jsx
+++ tictactoe/src/Td.jsx
@@ -4,9 +4,12 @@ import { CLICK_CELL, CHANGE_TURN } from './App';
 
 const Td = ({ rowIndex, cellIndex, dispatch, cellData }) => {
   const onClickTd = useCallback(() => {
+    if (cellData) {
+      return;
+    }
     dispatch({ type: CLICK_CELL, row: rowIndex, cell: cellIndex });
     dispatch({ type: CHANGE_TURN });
-  }, []);
+  }, [cellData]);
 
   return <td onClick={onClickTd}>{cellData}</td>;
 };
